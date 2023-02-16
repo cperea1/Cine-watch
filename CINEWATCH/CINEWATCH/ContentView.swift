ec5e672e19697bc334ab47436468275a0e5ba539
cperea1 <111528376+cperea1@users.noreply.github.com>
2023-02-15 17:38:54 -0700

    Add files via upload

---
+++ CINEWATCH/CINEWATCH/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "star")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("cinewatch")
+        }
+    }
+}
+
