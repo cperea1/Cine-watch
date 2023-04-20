261a6d9452f1ec5ba67c8017e2394f6164f2f82b
frankentank <54920881+frankentank@users.noreply.github.com>
2023-04-20 13:32:29 -0600

    idk what happened, but this works now

---
+++ app/src/main/java/com/example/cinewatch20/models/User.java
@@ -0,0 +1,169 @@
+package com.example.cinewatch20.models;
+
+import com.example.cinewatch20.data.MovieItem;
+import com.example.cinewatch20.utils.MovieDetailsServiceUtil;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+
+public class User implements Serializable {
+    private String id;
+    private String name;
+    private String age;
+    private String contact;
+    private String email;
+    private String username;
+    private String password;
+    private List<MovieModel> likedMovies = new ArrayList<>();
+    private List<MovieModel> dislikedMovies = new ArrayList<>();
+    private List<MovieModel> bookmarkedMovies = new ArrayList<>();
+
+    public User(){}
+
+    public User(String id, String name, String age, String contact, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+    }
+
+    public User(String id, String name, String username, String email, String password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public List<MovieModel> getLikedMovies() {
+        return likedMovies;
+    }
+
+
+    public void addLikedMovieItem(MovieModel movieModel) {
+        this.likedMovies.add(movieModel);
+    }
+
+
+
+    public void removeLikedMovieItem(int movieItemId){
+        this.likedMovies = this.likedMovies.stream()
+                .filter(movie -> movieItemId != movie.getId()).collect(Collectors.toList());
+    }
+
+
+
+    public void addAllLikedMovieItem(List<MovieModel> likedMovieItems) {
+
+        this.likedMovies.addAll(likedMovieItems);
+    }
+
+    public List<MovieModel> getDislikedMovies() {
+        return dislikedMovies;
+    }
+
+
+    public void addDislikedMovieItem(MovieModel movieModels) {
+        this.dislikedMovies.add(movieModels);
+    }
+
+    public void removeDislikedMovie(int movieItemId){
+        this.dislikedMovies = this.dislikedMovies.stream()
+                .filter(movie -> movieItemId != movie.getId()).collect(Collectors.toList());
+    }
+
+    public List<MovieModel> getBookmarkedMovies() {
+        return bookmarkedMovies;
+    }
+
+    public void addBookmarkedMovies(MovieModel movieModel) {
+        this.bookmarkedMovies.add(movieModel);
+
+    }
+
+    public void removeBookmarkedMovies(int movieItemId){
+        this.bookmarkedMovies = this.bookmarkedMovies.stream()
+                .filter(movie -> movieItemId != movie.getId()).collect(Collectors.toList());
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", contact='" + contact + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", dislikedMovies='" + dislikedMovies + '\'' +
+                ", bookmarkedMovies='" + bookmarkedMovies + '\'' +
+                ", likedMovies=" + likedMovies +
+                '}';
+    }
+}
